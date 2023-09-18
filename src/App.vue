8bb1c33b65f27d31fc07c5db6ae672f774bff06f
qian01_chen <qian01_chen@foxitsoftware.com>
2023-09-18 10:22:05 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -3,20 +3,7 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-<!--  <div>-->
-<!--    <a href="https://vitejs.dev" target="_blank">-->
-<!--      <img src="/vite.svg" class="logo" alt="Vite logo" />-->
-<!--    </a>-->
-<!--    <a href="https://vuejs.org/" target="_blank">-->
-<!--      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />-->
-<!--    </a>-->
-<!--  </div>-->
-<!--    <AppHeader />-->
     <RouterView />
-<!--  <router-link to="/foo">Go to Foo</router-link>-->
-<!--  <router-link to="/bar">Go to Bar</router-link>-->
-<!--  <router-view></router-view>-->
-<!--  <HelloWorld msg="Discover" />-->
 </template>
 
 <style scoped>
--- src/route/index.js
+++ src/route/index.js
@@ -1,9 +1,5 @@
 import { createRouter,createWebHashHistory} from "vue-router";
-// const home = () => import("../views/home")
-// const login = () => import("../views/login")
 import home from '../views/home.vue'
-// import login from '../views/login.vue'
-import compare from '../views/compare.vue'
 
 
 const routes = [
@@ -12,11 +8,6 @@ const routes = [
         path: "/home",
         name: "home",
         component: home
-    },
-    {
-        path: "/compare",
-        name: "compare",
-        component: compare
     }
 ]
 
